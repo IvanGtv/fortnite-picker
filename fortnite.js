9187a4a7a81c3f2ac6feaf24bf8b6e72e7129b78
IvanGtv <112436192+IvanGtv@users.noreply.github.com>
2023-06-14 14:23:19 -0500

    Add files via upload

---
+++ fortnite.js
@@ -0,0 +1,6 @@
+document.getElementById('one').onclick = partyOne
+
+function partyPurple() {
+    document.querySelector('slideshow').style.backgroundImage = "url(burger.png)";
+    document.querySelector('slideshow').style.color = 'white'
+  }
